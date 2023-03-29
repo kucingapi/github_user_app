1cb7f290b386ed00890c25bf6e4f44ea4b591ab4
novel bafagih <novelbafagih@student.ub.ac.id>
2023-03-30 04:49:32 +0700

    [ADD] SplashScreenActivity.kt for splash screen

---
+++ app/src/main/java/com/example/githubuser/activity/SplashScreenActivity.kt
@@ -0,0 +1,46 @@
+package com.example.githubuser.activity
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.WindowManager
+import com.example.githubuser.R
+import com.example.githubuser.databinding.ActivitySplashScreenBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        val view = binding.root
+        closeActionBar()
+        setContentView(view)
+        changeActivity()
+    }
+
+    private fun closeActionBar(){
+        if (Build.VERSION.SDK_INT < 16) {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        }
+        getSupportActionBar()?.hide();
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
+    }
+
+    private fun changeActivity(){
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
